5084347e6ef018d45e78c215949c78d97f18e77d
Sandley Guo <sandleyguo@gmail.com>
2019-02-26 18:28:26 -0500

    Add files via upload

--- wa_timetable.pl
+++ wa_timetable.pl
@@ -1,16 +1,37 @@
-# wa_timetable.pl
-# Sandley Guo
-#
+#Nicole Blanco and Sandley Guo
 
-open FILE, "<classV2.htm";
+#search
+sub search {
+	# Mechanize_webadvisor.pl - Try WWW::Mechanize with webadvisor.
 
-$classes = do { local $/; <FILE> };
+	use WWW::Mechanize;
 
+	$url = "https://www2.monmouth.edu/muwebadv/wa3/search/SearchClassesV2.aspx";
+	my $mech = WWW::Mechanize->new();
+	$mech->get($url);
+
+	#Select the term
+	$term = $ARGV[0];
+
+	$mech->field("_ctl0:MainContent:ddlTerm", $term);
+	$subject = $ARGV[1];
+
+	$mech->field("_ctl0:MainContent:ddlSubj_1", $subject);
+
+	#Click the Submit Button
+	$mech->click_button(name => "_ctl0:MainContent:btnSubmit");
+
+	#Get resulting html from clicking Submit. 
+	my $page = $mech->content();
+
+	print $page;
+} #end Search();
+
+#help
 sub help {
-	#Using a Perl "heredoc";
 	$message = <<"END_MSG";
 			
-			Term         : which Term (semester) the class is in, e.g. "19/SP - Spring 2019"
+			Term         : which Term (semester) the class is in, e.g. "19/SP - 2019 Spring"
 			Subject      : which Subject the class is in, e.g. English 
 			-help        : display a help screen and sample usage.
 			-terms       : list all currently available Terms
@@ -19,25 +40,62 @@ sub help {
 END_MSG
  
 	print $message;
-}		#end help();
+}#end help();
 
-#run help() if not enough command line args or if 1st arg contains -h:
-$numargs = $#ARGV + 1;
-if ($numargs < 2 || $ARGV[0] =~ /-h/) { #if $ARGV[0] contains -h
+
+#subjects
+sub subjects {
+	use WWW::Mechanize;
+
+	$url="https://www2.monmouth.edu/muwebadv/wa3/search/SearchClassesV2.aspx";
+	my $mech = WWW::Mechanize->new();
+	$mech->get($url);
+
+	my $page = $mech->content();
+
+	($subjects_section) = $page =~ /<select name="_ctl0:MainContent:ddlSubj_1"(.+?)<\/select>/s; 
+	
+	(@subjects_tag) = $subjects_section =~ /<option value="[A-Z]+">(.+?)<\/option>/g;
+
+	foreach $sub (@subjects_tag){
+		print ("$sub\n");
+	}
+}#end subjects();
+
+
+#terms
+sub terms {
+	use WWW::Mechanize;
+
+	$url=" https://www2.monmouth.edu/muwebadv/wa3/search/SearchClassesV2.aspx";
+	my $mech = WWW::Mechanize->new();
+	$mech -> get($url);
+
+	my $page = $mech -> content();
+
+	($terms_section) = $page =~ /<select name="_ctl0:MainContent:ddlTerm"(.+?)<\/select>/s; 
+	
+	(@terms_tag) = $terms_section =~ /<option value=".*">(.+?)<\/option>/g;
+
+	foreach $term (@terms_tag){
+		print ("$term\n");
+	}
+}#end terms();
+
+#run help() if not enough command line args or if 1st arg contains -help:
+if ($ARGV[0] =~ /-help/) { #if $ARGV[0] contains -help
 	help();
+	exit(1); #non-zero exit status means an error
+} elsif ($ARGV[0] =~ /-terms/) {
+	terms();
+	exit(1); #non-zero exit status means an error
+} elsif ($ARGV[0] =~ /-subjects/) {
+	subjects();
+	exit(1); #non-zero exit status means an error
+} else {
+	search();
 	exit(1); #non-zero exit status means an error
 }
 
 #Get sortby argument from first command line arg:
 my $sortby = shift @ARGV; 
-
-# Read all the headlines into @headkines:
-@term = $classes =~ /<optional>(.+)<\/optional>/g;
-# Each headline is (.+) between <optional> and </optional>.
-# Need to use () to exclude <optional> tags in match.
-# Must use \ in front of / in <optional>.
-
-# Display headlines:
-foreach my $h1 ( @optional ) {
-	print "$h1\n";
-}
